b4f0ffc651acb1243076ecbca9422d836e81f6b1
MAYERLY ANDREA <mayerlya.suarezl@unac.edu.co>
2023-03-15 15:52:19 -0500

    Cree nueva clase de Tiquete

---
+++ src/co/edu/unac/poo1/unidad2/aerolinea/TiqueteV2.java
@@ -0,0 +1,15 @@
+package co.edu.unac.poo1.unidad2.aerolinea;
+
+public class TiqueteV2 {
+
+    Tiquete Instanciapasajero_1 = new Tiquete("Andrea Suarez",1200000f,new Ruta("Bucaramanga","Bogotá"),null,TipoDePuesto.CLASE_ECONOMICA,"23E");
+    Tiquete Instanciapasajero_2 = new Tiquete("Ana Perez",1500000f,new Ruta("Bogotá","Cali"),null,TipoDePuesto.CLASE_EJECUTIVA,"56A");
+    Tiquete Instanciapasajero_3 = new Tiquete("Antonio gomez",1200000f,new Ruta("Medellín","Valledupar"),null,TipoDePuesto.CLASE_ECONOMICA,"23E");
+    Tiquete Instanciapasajero_4 = new Tiquete("Sofia Carson",1500000f,new Ruta("Bogotá","Bucaramanga"),null,TipoDePuesto.CLASE_EJECUTIVA,"56A");
+    Tiquete Instanciapasajero_5 = new Tiquete("William Suaz",1200000f,new Ruta("Medellín Bogotá","Bogotá"),null,TipoDePuesto.CLASE_ECONOMICA,"23E");
+    Tiquete Instanciapasajero_6 = new Tiquete("Roy Muñoz",1500000f,new Ruta("Bogotá","Cali"),null,TipoDePuesto.CLASE_EJECUTIVA,"56A");
+    Tiquete Instanciapasajero_7 = new Tiquete("Ester Corrales",1200000f,new Ruta("Medellín","Valledupar"),null,TipoDePuesto.CLASE_ECONOMICA,"23E");
+    Tiquete Instanciapasajero_8 = new Tiquete("Nelly Castro",1500000f,new Ruta("Bogotá","Bucaramanga"),null,TipoDePuesto.CLASE_EJECUTIVA,"56A");
+
+
+}
